37cf19c616cd14ac3e91189e91697851fb020b0c
James <wepudaljamesbernard05@gmail.com>
2024-06-04 23:22:16 +0800

    Authentication GUI (#7)
    added XML designs for Login and Register

--- app/src/main/java/com/example/hyperapp_dev/LibAuthentication/AuthActivities/Authentication.java
+++ app/src/main/java/com/example/hyperapp_dev/LibAuthentication/AuthActivities/Authentication.java
@@ -1,4 +1,4 @@
-package com.example.hyperapp_dev;
+package com.example.hyperapp_dev.LibAuthentication.AuthActivities;
 
 import android.os.Bundle;
 
@@ -8,13 +8,22 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.hyperapp_dev.R;
+
+public class Authentication extends AppCompatActivity {
+
+    /*
+    TODO: Reminder
+    This activity must have two fragments, Login and Register fragments.
+    Firebase authentication MAY be used here, or SharedPreferences (maybe), the former being
+    to comply with using Firebase as part of the task requirement
+     */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_authentication);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
---
+++ app/src/main/java/com/example/hyperapp_dev/LibAuthentication/AuthActivities/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.hyperapp_dev.LibAuthentication.AuthActivities;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.hyperapp_dev.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        toAuthenticationScreen();
+    }
+
+    // TODO: Remove and modify whatever is necessary (e.g. modify/delete the activity
+    // TODO: Whatever the changes is, be sure to check over the manifest file
+    // Do modify/omit this when the application has a flow and navigation
+    private void toAuthenticationScreen()
+    {
+        Intent intent = new Intent(MainActivity.this, Authentication.class);
+        startActivity(intent);
+    }
+}
