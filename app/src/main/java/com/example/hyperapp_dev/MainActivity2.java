d57b3a8cd86811a38ae465b8f5bb45ae4f8b55b1
Ali De Vera <alecsdevera@yahoo.com>
2024-06-13 11:08:58 +0800

    Splash Screen

--- app/src/main/java/com/example/hyperapp_dev/MainActivity2.java
+++ app/src/main/java/com/example/hyperapp_dev/MainActivity2.java
@@ -1,8 +1,6 @@
 package com.example.hyperapp_dev;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,25 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                startActivity(new Intent(MainActivity.this, MainActivity2.class));
-                finish();
-            }
-        }, 3000);
     }
 }
