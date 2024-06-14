05adae48a1315b1dd108691dc4d5269766a96000
TheDARKOEMTER <thebeangodzilla22@gmail.com>
2024-06-14 20:18:06 +0800

    Added component collection approach for mapbox

--- app/src/main/java/com/example/carcab/MapFeature/MapActivity.java
+++ app/src/main/java/com/example/carcab/MapFeature/MapActivity.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
@@ -14,20 +15,33 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.carcab.R;
+import com.mapbox.android.core.permissions.PermissionsManager;
+import com.mapbox.common.location.DeviceLocationProvider;
+import com.mapbox.common.location.DeviceLocationProviderFactory;
+import com.mapbox.common.location.LocationProvider;
+import com.mapbox.common.location.LocationService;
+import com.mapbox.common.location.LocationServiceFactory;
+import com.mapbox.maps.MapView;
+import com.mapbox.maps.MapboxMap;
+import com.mapbox.maps.plugin.locationcomponent.LocationCompassEngine;
+import com.mapbox.navigation.core.MapboxNavigation;
 
 import org.osmdroid.api.IMapController;
 import org.osmdroid.config.Configuration;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
-import org.osmdroid.views.MapView;
 import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
 import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
-
+import java.security.Permission;
 import java.util.ArrayList;
 
 public class MapActivity extends AppCompatActivity{
     private final int REQUEST_PERMISSIONS_REQUEST_CODE = 1;
-
+    private LocationProvider locationProvider;
+    private MapView mapView;
+    private MapboxMap map;
+    private PermissionsManager permissionsManager;
+    private LocationService locationService;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -38,6 +52,11 @@ public class MapActivity extends AppCompatActivity{
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        
+
+        mapView = findViewById(R.id.mapView);
+
+
     }
 
 
