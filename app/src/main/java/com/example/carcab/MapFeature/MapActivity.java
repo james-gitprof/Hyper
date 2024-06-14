d830e29e6402194f5379ad29439ec5ad7ba5e9fd
TheDARKOEMTER <thebeangodzilla22@gmail.com>
2024-06-14 21:41:27 +0800

    Added rendering of user's current location with a location component overlay (2d puck)

--- app/src/main/java/com/example/carcab/MapFeature/MapActivity.java
+++ app/src/main/java/com/example/carcab/MapFeature/MapActivity.java
@@ -1,48 +1,111 @@
 package com.example.carcab.MapFeature;
 
+import static com.mapbox.maps.plugin.gestures.GesturesUtils.getGestures;
+import static com.mapbox.maps.plugin.locationcomponent.LocationComponentUtils.getLocationComponent;
+
 import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.Camera;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.content.res.AppCompatResources;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.carcab.AuthenticateFeature.AuthenticationViews.AuthActivities.MainActivity;
 import com.example.carcab.R;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.mapbox.android.core.permissions.PermissionsListener;
 import com.mapbox.android.core.permissions.PermissionsManager;
+import com.mapbox.android.gestures.MoveGestureDetector;
+import com.mapbox.common.location.AccuracyAuthorization;
+import com.mapbox.common.location.AccuracyLevel;
 import com.mapbox.common.location.DeviceLocationProvider;
 import com.mapbox.common.location.DeviceLocationProviderFactory;
+import com.mapbox.common.location.IntervalSettings;
+import com.mapbox.common.location.Location;
+import com.mapbox.common.location.LocationObserver;
 import com.mapbox.common.location.LocationProvider;
+import com.mapbox.common.location.LocationProviderRequest;
 import com.mapbox.common.location.LocationService;
 import com.mapbox.common.location.LocationServiceFactory;
+import com.mapbox.common.location.LocationServiceObserver;
+import com.mapbox.common.location.PermissionStatus;
+import com.mapbox.geojson.Point;
+import com.mapbox.maps.CameraOptions;
+import com.mapbox.maps.Image;
+import com.mapbox.maps.ImageHolder;
 import com.mapbox.maps.MapView;
 import com.mapbox.maps.MapboxMap;
-import com.mapbox.maps.plugin.locationcomponent.LocationCompassEngine;
-import com.mapbox.navigation.core.MapboxNavigation;
+import com.mapbox.maps.Style;
+import com.mapbox.maps.plugin.LocationPuck;
+import com.mapbox.maps.plugin.LocationPuck2D;
+import com.mapbox.maps.plugin.gestures.OnMoveListener;
+import com.mapbox.maps.plugin.locationcomponent.LocationComponentPlugin;
+import com.mapbox.maps.plugin.locationcomponent.OnIndicatorBearingChangedListener;
+import com.mapbox.maps.plugin.locationcomponent.OnIndicatorPositionChangedListener;
 
-import org.osmdroid.api.IMapController;
-import org.osmdroid.config.Configuration;
-import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
-import org.osmdroid.util.GeoPoint;
-import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
-import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
+
 import java.security.Permission;
 import java.util.ArrayList;
+import java.util.List;
 
-public class MapActivity extends AppCompatActivity{
+public class MapActivity extends AppCompatActivity implements LocationServiceObserver, PermissionsListener, LocationObserver {
     private final int REQUEST_PERMISSIONS_REQUEST_CODE = 1;
-    private LocationProvider locationProvider;
     private MapView mapView;
-    private MapboxMap map;
+    private FloatingActionButton myLocation;
     private PermissionsManager permissionsManager;
-    private LocationService locationService;
 
+    private final OnIndicatorBearingChangedListener onIndicatorBearingChangedListener = new OnIndicatorBearingChangedListener() {
+        @Override
+        public void onIndicatorBearingChanged(double v) {
+            mapView.getMapboxMap().setCamera(new CameraOptions.Builder().bearing(v).build());
+        }
+    };
+
+    private final OnIndicatorPositionChangedListener onIndicatorPositionChangedListener = new OnIndicatorPositionChangedListener() {
+        @Override
+        public void onIndicatorPositionChanged(@NonNull Point point) {
+            mapView.getMapboxMap().setCamera(new CameraOptions.Builder().center(point).zoom(20.0).build());
+            getGestures(mapView).setFocalPoint(mapView.getMapboxMap().pixelForCoordinate(point));
+
+        }
+    };
+
+    private final OnMoveListener onMoveListener = new OnMoveListener() {
+        @Override
+        public void onMoveBegin(@NonNull MoveGestureDetector moveGestureDetector) {
+            getLocationComponent(mapView).removeOnIndicatorPositionChangedListener(onIndicatorPositionChangedListener);
+            getLocationComponent(mapView).removeOnIndicatorBearingChangedListener(onIndicatorBearingChangedListener);
+            getGestures(mapView).removeOnMoveListener(onMoveListener);
+            myLocation.show();
+        }
+
+        @Override
+        public boolean onMove(@NonNull MoveGestureDetector moveGestureDetector) {
+            return false;
+        }
+
+        @Override
+        public void onMoveEnd(@NonNull MoveGestureDetector moveGestureDetector) {
+
+        }
+    };
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,12 +115,94 @@ public class MapActivity extends AppCompatActivity{
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        
+
 
         mapView = findViewById(R.id.mapView);
+        myLocation = findViewById(R.id.fabMyLocation);
 
+        ActivityResultLauncher<String[]> locationPermissionRequest =
+                registerForActivityResult(new ActivityResultContracts.RequestMultiplePermissions(),
+                        result -> {
+                            Boolean fineLocationGranted = result.getOrDefault(
+                                    Manifest.permission.ACCESS_FINE_LOCATION, false);
+                            Boolean coarseLocationGranted = result.getOrDefault(
+                                    Manifest.permission.ACCESS_COARSE_LOCATION,false);
+                            if (fineLocationGranted != null && fineLocationGranted) {
+                                // Precise location access granted.
+                                Toast.makeText(this, "Precise location granted", Toast.LENGTH_SHORT).show();
+
+                            } else if (coarseLocationGranted != null && coarseLocationGranted) {
+                                // Only approximate location access granted.
+                                Toast.makeText(this, "Coarse location granted", Toast.LENGTH_SHORT).show();
+                            } else {
+                                // No location access granted.
+                                Toast.makeText(this, "Permission not grantd", Toast.LENGTH_SHORT).show();
+
+                            }
+                        });
+
+        locationPermissionRequest.launch(new String[] {
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+        });
+
+        mapView.getMapboxMap().loadStyle(Style.SATELLITE, new Style.OnStyleLoaded() {
+            @Override
+            public void onStyleLoaded(@NonNull Style style) {
+                mapView.getMapboxMap().setCamera(new CameraOptions.Builder().zoom(20.0).build());
+                LocationComponentPlugin locationComponentPlugin = getLocationComponent(mapView);
+                locationComponentPlugin.setEnabled(true);
+                LocationPuck2D locationPuck2d = new LocationPuck2D();
+                locationPuck2d.setBearingImage(ImageHolder.from(R.drawable.baseline_add_location_24));
+                locationComponentPlugin.setLocationPuck(locationPuck2d);
+                locationComponentPlugin.addOnIndicatorPositionChangedListener(onIndicatorPositionChangedListener);
+                locationComponentPlugin.addOnIndicatorBearingChangedListener(onIndicatorBearingChangedListener);
+                getGestures(mapView).addOnMoveListener(onMoveListener);
+
+                myLocation.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        locationComponentPlugin.addOnIndicatorBearingChangedListener(onIndicatorBearingChangedListener);
+                        locationComponentPlugin.addOnIndicatorPositionChangedListener(onIndicatorPositionChangedListener);
+                        getGestures(mapView).addOnMoveListener(onMoveListener);
+                        myLocation.hide();
+                    }
+                });
+            }
+        });
 
     }
 
 
+
+    @Override
+    public void onExplanationNeeded(@NonNull List<String> list) {
+
+    }
+
+    @Override
+    public void onPermissionResult(boolean b) {
+
+    }
+
+    @Override
+    public void onLocationUpdateReceived(@NonNull List<Location> locations) {
+        Log.d("Location", locations.toString());
+
+    }
+
+    @Override
+    public void onAvailabilityChanged(boolean isAvailable) {
+
+    }
+
+    @Override
+    public void onPermissionStatusChanged(@NonNull PermissionStatus permission) {
+
+    }
+
+    @Override
+    public void onAccuracyAuthorizationChanged(@NonNull AccuracyAuthorization authorization) {
+
+    }
 }
