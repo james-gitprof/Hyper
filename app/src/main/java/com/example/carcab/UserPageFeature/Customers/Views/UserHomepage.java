f85eac38c0d9b0daf316417cfd2edcd9232ad9f8
james <wepudaljamesbernard05@gmail.com>
2024-06-18 16:04:27 +0800

    fixing weird inset error

--- app/src/main/java/com/example/carcab/UserPageFeature/Customers/Views/UserHomepage.java
+++ app/src/main/java/com/example/carcab/UserPageFeature/Customers/Views/UserHomepage.java
@@ -17,7 +17,7 @@ public class UserHomepage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_userpage_customer);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.user_drawer_layout), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
