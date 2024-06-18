24db23946da855351947b07a3a1d5fab9a138aa5
james <wepudaljamesbernard05@gmail.com>
2024-06-18 16:25:43 +0800

    chore: build config configuration adjustments
    chore: added wait on all green config
    
    chore: edited releaser config
    
    chore: fixed typ0
    
    chore: removed npm dependency
    
    chore: optimization attempt
    
    removed minify

--- release.config.js
+++ release.config.js
@@ -3,11 +3,10 @@ const RELEASE_BRANCHES = ["master"];
 const RELEASE_FILES = ["*.apk"]
 const MESSAGE_FORMAT = "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
 
-export const config = {
-    branches: [RELEASE_BRANCHES],
+const config = {
+    branches: RELEASE_BRANCHES,
     plugins: ['@semantic-release/commit-analyzer',
         '@semantic-release/release-notes-generator',
-        '@semantic-release/npm',
         '@semantic-release/github',
         ['@semantic-release/git',
             {"assets": RELEASE_FILES,
@@ -15,3 +14,5 @@ export const config = {
             }
         ]]
 };
+
+module.exports = config;
